9554cb27604ff4b078e6246b91115ea5873a81ab
Büşra Sude Külek <bkulek@deloitte.com>
2025-06-14 22:41:54 +0300

    tf.json path changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,11 +178,6 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "ssm" {
-  role       = aws_iam_role.jenkins_terraform_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
-}
-
 resource "aws_iam_role_policy" "jenkins_inline_policy" {
   name = "jenkins-inline-policy"
   role = aws_iam_role.jenkins_terraform_role.id
