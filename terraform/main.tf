ee2e7c2c917c77e1ec6e0dabcc6b7138950021dc
Büşra Sude Külek <bkulek@deloitte.com>
2025-06-17 23:03:34 +0300

    ip update

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,14 +113,14 @@ resource "aws_security_group" "jenkins_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["163.116.178.195/32"]  # 
+    cidr_blocks = ["81.215.238.43/32"]  # 
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["163.116.178.195/32"]  # 
+    cidr_blocks = ["81.215.238.43/32"]  # 
   }
 
 
@@ -128,21 +128,21 @@ resource "aws_security_group" "jenkins_sg" {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
-    cidr_blocks = ["163.116.178.195/32"]  # 
+    cidr_blocks = ["81.215.238.43/32"]  # 
   }
 
    ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["163.116.178.195/32"]  # 
+    cidr_blocks = ["81.215.238.43/32"]  # 
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["163.116.178.195/32"]  # 
+    cidr_blocks = ["81.215.238.43/32"]  # 
   }
 
   egress {
