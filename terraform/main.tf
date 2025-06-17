4cb880305437403e3f4caeb15be9123c2432910b
Büşra Sude Külek <bkulek@deloitte.com>
2025-06-17 23:16:43 +0300

    ssm iam role added

--- terraform/main.tf
+++ terraform/main.tf
@@ -232,6 +232,12 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.jenkins_terraform_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
 resource "aws_iam_role_policy" "jenkins_inline_policy" {
   name = "jenkins-inline-policy"
   role = aws_iam_role.jenkins_terraform_role.id
